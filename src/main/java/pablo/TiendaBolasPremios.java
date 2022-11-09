0134d989c04e3b06bc5f30fedea10322b123af4e
Pablodaw2022 <pnavgarb@g.educaand.es>
2022-11-09 20:26:59 +0100

    finalizacion ejercicio tienda Bolas

--- src/main/java/pablo/TiendaBolasPremios.java
+++ src/main/java/pablo/TiendaBolasPremios.java
@@ -7,7 +7,6 @@ package pablo;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,24 +16,31 @@ public class TiendaBolasPremios {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Scanner teclado = new Scanner(System.in);
+
         //variables
         int bola;
         double devolverImporte;
         String menu = "";
-       
+        double dinero = 0;
+        double precioFinal;
+        //Meotods
         menu = mostrarMenu();
-        bola=generarBola();
-        devolverImporte=importeCompra();
-        descuentoBola(bola, dinero);
+
+        bola = generarBola();
+
+        devolverImporte = importeCompra();
+
+        precioFinal = descuentoPrecio(bola,devolverImporte);
     }
 
     public static String mostrarMenu() {
 
         String texto = """
+                     -----------------------------------------------------
                       Bienvenido a al sorteo de nuestra tienda
-                      Una ves saques la bola di de que color es
-                     
+                      Una ves saques la bola segun el colo obtendra una 
+                      serie de descuentos
+                     -----------------------------------------------------
                     """;
 
         System.out.println(texto);
@@ -44,24 +50,42 @@ public class TiendaBolasPremios {
     }
 
     public static int generarBola() {
-
         Random numeroAleatorio = new Random();
 
         int numeroBola = numeroAleatorio.nextInt(4) + 1;
-        System.out.println("Has sacado el " + numeroBola);
 
-        if (numeroBola == 1) {
+        do {
 
-            System.out.println("Has sacado la bola azul,obtienes un 20% de descuento");
-        } else if (numeroBola == 2) {
-            System.out.println("Has sacado la bola roja,obtienes un 25% de descuento");
-        } else if (numeroBola == 3) {
+            if (numeroBola == 1) {
 
-            System.out.println("Has sacado la bola blanca, no tienes descuento");
-        } else if (numeroBola == 4) {
+                System.out.println("Has sacado la bola azul,obtienes un 20% de descuento");
 
-            System.out.println("Has sacado la boola verde, vuelves a sacar otra bola");
-        }
+            } else if (numeroBola == 2) {
+
+                System.out.println("Has sacado la bola roja,obtienes un 25% de descuento");
+
+            } else if (numeroBola == 3) {
+
+                System.out.println("Has sacado la bola blanca, no tienes descuento");
+            } else if (numeroBola == 4) {
+                numeroBola = numeroAleatorio.nextInt(3) + 1;
+
+                System.out.println("Has sacado la boola verde, vuelves a sacar otra bola");
+
+                if (numeroBola == 1) {
+
+                    System.out.println(" Has sacado la bola azul,,obtienes un 20% de descuento");
+                } else if (numeroBola == 2) {
+                    System.out.println(" Has sacado la bola roja,obtienes un 25% de descuento");
+
+                } else if (numeroBola == 3) {
+
+                    System.out.println("Has sacado la bola blanca, no tienes descuento");
+                }
+
+            }
+
+        } while (numeroBola == 4);
 
         return numeroBola;
     }
@@ -70,40 +94,46 @@ public class TiendaBolasPremios {
         Scanner teclado = new Scanner(System.in);
         double importe = 0;
         boolean repetir = true;
-        do {
-            try {
-                System.out.println("Introduzca el importe de su compra por favor");
+        do{
+        try {
+            System.out.println("Introduzca el importe de su compra por favor");
 
-                importe = teclado.nextDouble();
+            importe = teclado.nextDouble();
 
-                System.out.println(" El importe de tu compra es de  " + importe);
-                repetir = false;
-            } catch (InputMismatchException ime) {
-                System.out.println(" Precio no valido, vuelva a introducir el numero");
-                teclado.nextLine();
-            }
-        } while (repetir);
-
+            System.out.println(" El importe de tu compra es de  " + importe);
+            repetir = false;
+        } catch (InputMismatchException ime) {
+            System.out.println(" Precio no valido, vuelva a introducir el numero");
+            teclado.nextLine();
+        }
+        }while(repetir);
         return importe;
     }
 
-    public static double descuentoBola(int numeroBola, double importe) {
+    public static double descuentoPrecio(int numeroBola,double importe) {
         double precioFinal = 0;
+        
+        
 
         if (numeroBola == 1) {
 
             precioFinal = (importe * 0.2);
-            System.out.println("El precio a pagar al final es de " + precioFinal);
+            System.out.println("El precio que te descuentan es de " + precioFinal+"€");
         } else if (numeroBola == 2) {
             precioFinal = (importe * 0.25);
-            System.out.println("El precio a pagar al final es de " + precioFinal);
+            System.out.println("El precio que te descontan es de   " + precioFinal+"€");
         } else if (numeroBola == 3) {
 
             System.out.println("No tienes descuento sigues pagando  " + importe);
-        } else if (numeroBola == 4) {
         }
 
         return precioFinal;
     }
-
+    
+    
+    
+    
+    
+    
+  
 }
